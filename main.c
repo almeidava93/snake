5a2c276ea181d71f5bd49a0db8b6d696ffa079a8
almeidava93 <88211872+almeidava93@users.noreply.github.com>
2026-09-10 19:36:53 -0300

    clang-format

--- main.c
+++ main.c
@@ -1,63 +1,25 @@
-/*******************************************************************************************
- *
- *   raylib [core] example - basic window
- *
- *   Example complexity rating: [★☆☆☆] 1/4
- *
- *   Welcome to raylib!
- *
- *   To test examples, just press F6 and execute 'raylib_compile_execute' script
- *   Note that compiled executable is placed in the same folder as .c file
- *
- *   To test the examples on Web, press F6 and execute
- * 'raylib_compile_execute_web' script Web version of the program is generated
- * in the same folder as .c file
- *
- *   You can find all basic examples on C:\raylib\raylib\examples folder or
- *   raylib official webpage: www.raylib.com
- *
- *   Enjoy using raylib. :)
- *
- *   Example originally created with raylib 1.0, last time updated with
- * raylib 1.0
- *
- *   Example licensed under an unmodified zlib/libpng license, which is an
- * OSI-certified, BSD-like license that allows static linking with closed source
- * software
- *
- *   Copyright (c) 2013-2026 Ramon Santamaria (@raysan5)
- *
- ********************************************************************************************/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 #include "raylib.h"
 
 const int screenWidth = 800;
 const int screenHeight = 450;
-const int targetFPS = 60; // Target frames-per-second
-const char *windowTitle = "Snake";
-const int initPlayerLifes = 3; // Starting number of player lifes
+const int targetFPS = 60;  // Target frames-per-second
+const char* windowTitle = "Snake";
+const int initPlayerLifes = 3;  // Starting number of player lifes
 
 // Player structure
-typedef enum PlayerDirection
-{
-  LEFT,
-  RIGHT,
-  UP,
-  DOWN
-} PlayerDirection;
+typedef enum PlayerDirection { LEFT, RIGHT, UP, DOWN } PlayerDirection;
 
-typedef struct BodyPartState
-{
+typedef struct BodyPartState {
   int x;
   int y;
   PlayerDirection direction;
 } BodyPartState;
 
-typedef struct Player
-{
+typedef struct Player {
   Vector2 position;
   Vector2 stepSize;
   int framesPerStep;
@@ -75,29 +37,22 @@ typedef struct Player
 // Food, obstacles and other elements
 //------------------------------------------------------------------------------------
 
-typedef enum FoodType
-{
-  APPLE_FRUIT,
-  ORANGE_FRUIT,
-  BANANA_FRUIT
-} FoodType;
+typedef enum FoodType { APPLE_FRUIT, ORANGE_FRUIT, BANANA_FRUIT } FoodType;
 
-typedef struct Food
-{
+typedef struct Food {
   Vector2 position;
   Vector2 size;
   FoodType type;
 } Food;
 
-Food *create_new_food(void)
-{
-  Food *food = calloc(1, sizeof(Food));
+Food* create_new_food(void) {
+  Food* food = calloc(1, sizeof(Food));
   food->size.x = 20;
   food->size.y = 20;
   food->type = APPLE_FRUIT;
 
   // Generate random position for the food
-  srand(time(NULL)); // seed random number generator with current time
+  srand(time(NULL));  // seed random number generator with current time
   int min = 0;
   int max_x = screenWidth - food->size.x;
   int max_y = screenHeight - food->size.y;
@@ -108,34 +63,28 @@ Food *create_new_food(void)
   return food;
 }
 
-void free_food(Food *food)
-{
-  if (food == NULL)
-  {
+void free_food(Food* food) {
+  if (food == NULL) {
     return;
   }
   free(food);
 }
 
-void draw_food(Food *food)
-{
-  if (food == NULL)
-    return;
-  DrawRectangle(food->position.x, food->position.y, food->size.x,
-                food->size.y, RED);
+void draw_food(Food* food) {
+  if (food == NULL) return;
+  DrawRectangle(food->position.x, food->position.y, food->size.x, food->size.y,
+                RED);
 }
 
 //------------------------------------------------------------------------------------
 // Player functions
 //------------------------------------------------------------------------------------
 
-Player *create_new_player(Player playerReference)
-{
-  Player *player = calloc(1, sizeof(Player));
+Player* create_new_player(Player playerReference) {
+  Player* player = calloc(1, sizeof(Player));
   *player = playerReference;
 
-  for (size_t i = 0; i < player->numBodyParts; i++)
-  {
+  for (size_t i = 0; i < player->numBodyParts; i++) {
     player->bodyPartsStates[i] =
         (BodyPartState){.x = player->position.x + player->size.x * (i + 1),
                         .y = player->position.y,
@@ -145,12 +94,10 @@ Player *create_new_player(Player playerReference)
   return player;
 }
 
-void reset_player(Player *player, Player playerReference)
-{
+void reset_player(Player* player, Player playerReference) {
   *player = playerReference;
 
-  for (size_t i = 0; i < player->numBodyParts; i++)
-  {
+  for (size_t i = 0; i < player->numBodyParts; i++) {
     player->bodyPartsStates[i] =
         (BodyPartState){.x = player->position.x + player->size.x * (i + 1),
                         .y = player->position.y,
@@ -158,97 +105,81 @@ void reset_player(Player *player, Player playerReference)
   }
 }
 
-void free_player(Player *player) { free(player); }
+void free_player(Player* player) { free(player); }
 
-void move_head(Player *player)
-{
-  switch (player->direction)
-  {
-  case LEFT:
-    player->position.x -= player->stepSize.x;
-    break;
-  case RIGHT:
-    player->position.x += player->stepSize.x;
-    break;
-  case UP:
-    player->position.y -= player->stepSize.y;
-    break;
-  case DOWN:
-    player->position.y += player->stepSize.y;
-    break;
+void move_head(Player* player) {
+  switch (player->direction) {
+    case LEFT:
+      player->position.x -= player->stepSize.x;
+      break;
+    case RIGHT:
+      player->position.x += player->stepSize.x;
+      break;
+    case UP:
+      player->position.y -= player->stepSize.y;
+      break;
+    case DOWN:
+      player->position.y += player->stepSize.y;
+      break;
   }
 }
 
-void update_player_direction(Player *player)
-{
-  if (IsKeyDown(KEY_LEFT) && player->direction != RIGHT)
-  {
+void update_player_direction(Player* player) {
+  if (IsKeyDown(KEY_LEFT) && player->direction != RIGHT) {
     player->direction = LEFT;
-  }
-  else if (IsKeyDown(KEY_RIGHT) && player->direction != LEFT)
-  {
+  } else if (IsKeyDown(KEY_RIGHT) && player->direction != LEFT) {
     player->direction = RIGHT;
-  }
-  else if (IsKeyDown(KEY_UP) && player->direction != DOWN)
-  {
+  } else if (IsKeyDown(KEY_UP) && player->direction != DOWN) {
     player->direction = UP;
-  }
-  else if (IsKeyDown(KEY_DOWN) && player->direction != UP)
-  {
+  } else if (IsKeyDown(KEY_DOWN) && player->direction != UP) {
     player->direction = DOWN;
   }
 }
 
-void move_player(Player *player)
-{
-  if (player->collided)
-  {
+void move_player(Player* player) {
+  if (player->collided) {
     return;
   }
 
   // Save the head's current position before it moves
-  BodyPartState prevBodyPartState = {
-      .x = player->position.x,
-      .y = player->position.y,
-      .direction = player->direction};
+  BodyPartState prevBodyPartState = {.x = player->position.x,
+                                     .y = player->position.y,
+                                     .direction = player->direction};
 
   // Move the head
   move_head(player);
 
   // Update body parts' position
   BodyPartState tempBodyPartState;
-  for (size_t body_part_idx = 0; body_part_idx < player->numBodyParts; body_part_idx++)
-  {
+  for (size_t body_part_idx = 0; body_part_idx < player->numBodyParts;
+       body_part_idx++) {
     tempBodyPartState = player->bodyPartsStates[body_part_idx];
 
     // Move this body part into into the previous part position
     player->bodyPartsStates[body_part_idx].x = prevBodyPartState.x;
     player->bodyPartsStates[body_part_idx].y = prevBodyPartState.y;
-    player->bodyPartsStates[body_part_idx].direction = prevBodyPartState.direction;
+    player->bodyPartsStates[body_part_idx].direction =
+        prevBodyPartState.direction;
 
     // Update previous body part position
     prevBodyPartState = tempBodyPartState;
   }
 
-  if (player->bodyPartsToAdd > 0)
-  {
+  if (player->bodyPartsToAdd > 0) {
     player->bodyPartsStates[player->numBodyParts] = prevBodyPartState;
     player->bodyPartsToAdd--;
     player->numBodyParts++;
   }
 }
 
-void checkCollisions(Player *player)
-{
-  if (player->collided)
-  {
+void checkCollisions(Player* player) {
+  if (player->collided) {
     return;
   }
 
   // Check collision with screen bounds
   if (player->position.x > screenWidth || player->position.x < 0 ||
-      player->position.y > screenHeight || player->position.y < 0)
-  {
+      player->position.y > screenHeight || player->position.y < 0) {
     player->collided = true;
   }
 
@@ -256,33 +187,25 @@ void checkCollisions(Player *player)
   Rectangle head = {player->position.x, player->position.y, player->size.x,
                     player->size.y};
   for (size_t body_part_idx = 0; body_part_idx < player->numBodyParts;
-       body_part_idx++)
-  {
+       body_part_idx++) {
     BodyPartState currentBodyPartState = player->bodyPartsStates[body_part_idx];
     Rectangle currentBodyPartRectangle = {currentBodyPartState.x,
                                           currentBodyPartState.y,
                                           player->size.x, player->size.y};
-    if (CheckCollisionRecs(head, currentBodyPartRectangle))
-    {
+    if (CheckCollisionRecs(head, currentBodyPartRectangle)) {
       player->collided = true;
       break;
     }
   }
 }
 
-void checkIfFoundFood(Player *player, Food *food, bool *foodIsAvailable)
-{
-  if (food == NULL)
-    return;
+void checkIfFoundFood(Player* player, Food* food, bool* foodIsAvailable) {
+  if (food == NULL) return;
   Rectangle head = {player->position.x, player->position.y, player->size.x,
                     player->size.y};
-  Rectangle foodRect = {
-      food->position.x,
-      food->position.y,
-      food->size.x,
-      food->size.y};
-  if (CheckCollisionRecs(head, foodRect))
-  {
+  Rectangle foodRect = {food->position.x, food->position.y, food->size.x,
+                        food->size.y};
+  if (CheckCollisionRecs(head, foodRect)) {
     player->bodyPartsToAdd++;
     free_food(food);
     *foodIsAvailable = false;
@@ -293,9 +216,8 @@ void checkIfFoundFood(Player *player, Food *food, bool *foodIsAvailable)
 // Drawing functions
 //------------------------------------------------------------------------------------
 
-void DrawTextCentered(const char *text, int centerY, int fontSize,
-                      Color color)
-{
+void DrawTextCentered(const char* text, int centerY, int fontSize,
+                      Color color) {
   int textWidth = MeasureText(text, fontSize);
   int posX = (GetScreenWidth() / 2) - (textWidth / 2);
   DrawText(text, posX, centerY, fontSize, color);
@@ -305,14 +227,13 @@ void DrawTextCentered(const char *text, int centerY, int fontSize,
 // Program main entry point
 //------------------------------------------------------------------------------------
 
-int main(void)
-{
+int main(void) {
   // Initialization
   //--------------------------------------------------------------------------------------
 
   int framesCounter = 0;
   bool foodIsAvailable = false;
-  Food *currentFood;
+  Food* currentFood;
 
   // Initialize player
   Player startingPlayerReference = {
@@ -325,22 +246,18 @@ int main(void)
       .collided = false,
       .direction = LEFT,
       .bodyPartsToAdd = 0};
-  Player *player = create_new_player(startingPlayerReference);
+  Player* player = create_new_player(startingPlayerReference);
 
-  typedef enum GameScreen
-  {
-    GAMEPLAY,
-    GAMEOVER
-  } GameScreen;
+  typedef enum GameScreen { GAMEPLAY, GAMEOVER } GameScreen;
 
   InitWindow(screenWidth, screenHeight, windowTitle);
   GameScreen currentGameScreen = GAMEPLAY;
 
-  SetTargetFPS(targetFPS); // Set our game to run at 60 frames-per-second
+  SetTargetFPS(targetFPS);  // Set our game to run at 60 frames-per-second
   //--------------------------------------------------------------------------------------
 
   // Main game loop
-  while (!WindowShouldClose()) // Detect window close button or ESC key
+  while (!WindowShouldClose())  // Detect window close button or ESC key
   {
     // Update
     //----------------------------------------------------------------------------------
@@ -352,70 +269,63 @@ int main(void)
     framesCounter++;
     float deltaTime = GetFrameTime();
 
-    switch (currentGameScreen)
-    {
-    case GAMEPLAY:
-      if (foodIsAvailable == false)
-      {
-        currentFood = create_new_food();
-        foodIsAvailable = true;
-      }
-
-      update_player_direction(player);
-      if (framesCounter % player->framesPerStep == 0)
-      {
-        move_player(player);
-        checkCollisions(player);
-        checkIfFoundFood(player, currentFood, &foodIsAvailable);
-
-        if (player->collided)
-        {
-          currentGameScreen = GAMEOVER;
+    switch (currentGameScreen) {
+      case GAMEPLAY:
+        if (foodIsAvailable == false) {
+          currentFood = create_new_food();
+          foodIsAvailable = true;
         }
-        break;
-      }
 
-    case GAMEOVER:
-      if (IsKeyDown(KEY_ENTER))
-      {
-        currentGameScreen = GAMEPLAY;
-        reset_player(player, startingPlayerReference);
-      }
+        update_player_direction(player);
+        if (framesCounter % player->framesPerStep == 0) {
+          move_player(player);
+          checkCollisions(player);
+          checkIfFoundFood(player, currentFood, &foodIsAvailable);
+
+          if (player->collided) {
+            currentGameScreen = GAMEOVER;
+          }
+          break;
+        }
+
+      case GAMEOVER:
+        if (IsKeyDown(KEY_ENTER)) {
+          currentGameScreen = GAMEPLAY;
+          reset_player(player, startingPlayerReference);
+        }
     }
 
     // Draw
     BeginDrawing();
 
     // Check current screen and draw it
-    switch (currentGameScreen)
-    {
-    case GAMEPLAY:
-      ClearBackground(RAYWHITE);
-      DrawText(TextFormat("FPS: %d", GetFPS()), 10, 10, 20, DARKGRAY);
+    switch (currentGameScreen) {
+      case GAMEPLAY:
+        ClearBackground(RAYWHITE);
+        DrawText(TextFormat("FPS: %d", GetFPS()), 10, 10, 20, DARKGRAY);
 
-      // Draw snake
-      DrawRectangle(player->position.x, player->position.y, player->size.x,
-                    player->size.y, BLACK);
-      for (size_t body_part_idx = 0; body_part_idx < player->numBodyParts;
-           body_part_idx++)
-      {
-        BodyPartState currentBodyPartState =
-            player->bodyPartsStates[body_part_idx];
-        DrawRectangle(currentBodyPartState.x, currentBodyPartState.y,
-                      player->size.x, player->size.y, BLACK);
-      }
+        // Draw snake
+        DrawRectangle(player->position.x, player->position.y, player->size.x,
+                      player->size.y, BLACK);
+        for (size_t body_part_idx = 0; body_part_idx < player->numBodyParts;
+             body_part_idx++) {
+          BodyPartState currentBodyPartState =
+              player->bodyPartsStates[body_part_idx];
+          DrawRectangle(currentBodyPartState.x, currentBodyPartState.y,
+                        player->size.x, player->size.y, BLACK);
+        }
 
-      // Draw food
-      draw_food(currentFood);
+        // Draw food
+        draw_food(currentFood);
 
-      break;
+        break;
 
-    case GAMEOVER:
-      ClearBackground(RAYWHITE);
-      DrawTextCentered("GAME OVER", screenHeight / 2, 20, DARKGRAY);
-      DrawTextCentered("PRESS ENTER TO RESTART", screenHeight / 2 + 25, 20,
-                       DARKGRAY);
-      break;
+      case GAMEOVER:
+        ClearBackground(RAYWHITE);
+        DrawTextCentered("GAME OVER", screenHeight / 2, 20, DARKGRAY);
+        DrawTextCentered("PRESS ENTER TO RESTART", screenHeight / 2 + 25, 20,
+                         DARKGRAY);
+        break;
     }
 
     EndDrawing();
@@ -424,7 +334,7 @@ int main(void)
 
   // De-Initialization
   //--------------------------------------------------------------------------------------
-  CloseWindow(); // Close window and OpenGL context
+  CloseWindow();  // Close window and OpenGL context
   free_player(player);
   free_food(currentFood);
   //--------------------------------------------------------------------------------------
